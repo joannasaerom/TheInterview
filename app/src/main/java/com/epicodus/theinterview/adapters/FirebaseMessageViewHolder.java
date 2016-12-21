a36cd3d73621bb5d57af40f8067045931016bcc7
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-12-21 11:52:50 -0800

    Switch playback image to toggle button and set onclicklistener

--- app/src/main/java/com/epicodus/theinterview/adapters/FirebaseMessageViewHolder.java
+++ app/src/main/java/com/epicodus/theinterview/adapters/FirebaseMessageViewHolder.java
@@ -3,17 +3,22 @@ package com.epicodus.theinterview.adapters;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.epicodus.theinterview.R;
 import com.epicodus.theinterview.models.Message;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by joannaanderson on 12/21/16.
  */
 
 public class FirebaseMessageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    @Bind(R.id.playback) ToggleButton mPlaybackImage;
 
     View mView;
     Context mContext;
@@ -22,10 +27,12 @@ public class FirebaseMessageViewHolder extends RecyclerView.ViewHolder implement
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
+        ButterKnife.bind(this, itemView);
+
+        mPlaybackImage.setOnClickListener(this);
     }
 
     public void bindMessage(Message message){
-        ImageView mPlaybackImage = (ImageView) mView.findViewById(R.id.playbackImage);
         TextView mTimestamp = (TextView) mView.findViewById(R.id.timestamp);
 
         mTimestamp.setText("Sent:" + message.getTimestamp());
@@ -33,6 +40,13 @@ public class FirebaseMessageViewHolder extends RecyclerView.ViewHolder implement
 
     @Override
     public void onClick(View v){
+        if (v == mPlaybackImage) {
+            if (((ToggleButton) v).isChecked()){
+                Toast.makeText(mContext, "Play", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(mContext, "Pause", Toast.LENGTH_SHORT).show();
+            }
 
+        }
     }
 }
