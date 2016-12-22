8d5890015f518367c45ab246a1fb1d2ce6be80c6
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-12-21 21:43:20 -0800

    Add menu to Feedback activity

--- app/src/main/java/com/epicodus/theinterview/ui/FeedbackActivity.java
+++ app/src/main/java/com/epicodus/theinterview/ui/FeedbackActivity.java
@@ -1,9 +1,13 @@
 package com.epicodus.theinterview.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.epicodus.theinterview.Constants;
@@ -43,6 +47,27 @@ public class FeedbackActivity extends AppCompatActivity {
         setUpFirebaseAdapter();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_feedback, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_logout){
+            logout();
+            return true;
+        }
+        if (id == R.id.interviews){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void setUpFirebaseAdapter(){
         mFirebaseAdapter = new FirebaseRecyclerAdapter<Feedback, FirebaseFeedbackViewHolder>(Feedback.class, R.layout.feedback_list_item, FirebaseFeedbackViewHolder.class, mFeedbackReference) {
             @Override
@@ -53,5 +78,13 @@ public class FeedbackActivity extends AppCompatActivity {
         mFeedbackList.setHasFixedSize(true);
         mFeedbackList.setLayoutManager(new LinearLayoutManager(this));
         mFeedbackList.setAdapter(mFirebaseAdapter);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(FeedbackActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
