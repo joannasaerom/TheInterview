b17477e9025d730c9a1411f2568b5f49bd061243
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-12-21 21:19:06 -0800

    Create firebase adapter for feedback and display feedback for user

--- app/src/main/java/com/epicodus/theinterview/adapters/FirebaseFeedbackViewHolder.java
+++ app/src/main/java/com/epicodus/theinterview/adapters/FirebaseFeedbackViewHolder.java
@@ -1,8 +1,34 @@
 package com.epicodus.theinterview.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.epicodus.theinterview.R;
+import com.epicodus.theinterview.models.Feedback;
+
+
 /**
  * Created by joannaanderson on 12/21/16.
  */
 
-public class FirebaseFeedbackViewHolder {
+public class FirebaseFeedbackViewHolder extends RecyclerView.ViewHolder {
+    View mView;
+    Context mContext;
+
+    public FirebaseFeedbackViewHolder(View itemView){
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+    }
+
+    public void bindFeedback(Feedback feedback){
+        TextView mFeedback = (TextView) mView.findViewById(R.id.feedback);
+        TextView mTimestamp = (TextView) mView.findViewById(R.id.timestamp);
+
+        mFeedback.setText(feedback.getTextBody());
+        mTimestamp.setText("Date: " + feedback.getTimestamp());
+
+    }
 }
--- app/src/main/java/com/epicodus/theinterview/ui/FeedbackActivity.java
+++ app/src/main/java/com/epicodus/theinterview/ui/FeedbackActivity.java
@@ -2,10 +2,19 @@ package com.epicodus.theinterview.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.epicodus.theinterview.Constants;
 import com.epicodus.theinterview.R;
+import com.epicodus.theinterview.adapters.FirebaseFeedbackViewHolder;
+import com.epicodus.theinterview.models.Feedback;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,10 +23,35 @@ public class FeedbackActivity extends AppCompatActivity {
     @Bind(R.id.feedbackList) RecyclerView mFeedbackList;
     @Bind(R.id.activityTitle) TextView mActivityTitle;
 
+    private DatabaseReference mFeedbackReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         ButterKnife.bind(this);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mFeedbackReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_FEEDBACK_REFERENCE)
+                .child(uid);
+
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter(){
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Feedback, FirebaseFeedbackViewHolder>(Feedback.class, R.layout.feedback_list_item, FirebaseFeedbackViewHolder.class, mFeedbackReference) {
+            @Override
+            protected void populateViewHolder(FirebaseFeedbackViewHolder viewHolder, Feedback model, int position) {
+                viewHolder.bindFeedback(model);
+            }
+        };
+        mFeedbackList.setHasFixedSize(true);
+        mFeedbackList.setLayoutManager(new LinearLayoutManager(this));
+        mFeedbackList.setAdapter(mFirebaseAdapter);
     }
 }
